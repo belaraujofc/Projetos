7ae8a111fdc311f90bc7038f15ac66ef4cac156c
belaraujofc <belaraujofc@gmail.com>
2025-09-23 23:08:36 -0300

    novo commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using PROJETOS.Models;
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+
+
+
+//string apresentacao = "Olá, seja bem-vindo ao curso de C#!";
+
+// int quantidade = 1;
+// Console.WriteLine(" Valor da variavel quantidade: " + quantidade);
+//  quantidade= 10;
+//  Console.WriteLine(" Valor da variavel quantidade: " + quantidade  );
+//double altura = 1.75;
+//bool condicao = true;
+//decimal preco = 19.99m;
+
+
+//Console.WriteLine("Valor da variavel apresentacao: " + apresentacao);
+//Console.WriteLine(" Valor da variavel quantidade: " + quantidade  );
+//Console.WriteLine(" Valor da variavel altura: " + altura.ToString("0.00"));
+//Console.WriteLine(" Valor da variavel condicao: " + condicao);
+//Console.WriteLine(" Valor da variavel preco: " + preco);
+
+
+
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "João";
+//pessoa1.Idade = 25;
+//pessoa1.Apresentar();
